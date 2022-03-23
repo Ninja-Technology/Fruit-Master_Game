29e0087e2ed9acb734cff233c149ca1fc4e36f78
Elliot Bazuaye <obazuaye55@gmail.com>
2022-03-23 02:27:03 -0600

    added Voices and Sounds, Collission - Lobby Scene

---
+++ Assets/Ninja Fruit Games/Scripts/DelaySound.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelaySound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.welcomeAndInstructionVoice.gameObject.transform.position = transform.position;
+        AudioManager.instance.welcomeAndInstructionVoice.PlayDelayed(8);
+    }
+
+
+}
